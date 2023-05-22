09d42d416f6f6f20fd9d60a454da64eb42bf33b0
Diego <diego.valdez.r@tecsup.edu.pe>
2023-05-21 22:28:30 -0500

    first commit

---
+++ DependencyService/DependencyService.Android/Services/QrScanningService.cs
@@ -0,0 +1,26 @@
+﻿using DependencyService.Services;
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using ZXing.Mobile;
+
+[assembly: Dependency(typeof(DependencyService.Droid.Services.QrScanningService))]
+namespace DependencyService.Droid.Services
+{
+    public class QrScanningService : IQrScanningService
+    {
+        public async Task<string> ScanAsync()
+        {
+            var options = new MobileBarcodeScanningOptions();
+            var scanner = new MobileBarcodeScanner();
+
+            var context = Xamarin.Essentials.Platform.CurrentActivity;  
+
+            var scanResult = await scanner.Scan(context, options);
+            return scanResult.Text;
+        }
+    }
+}
---
+++ DependencyService/DependencyService/TextToSpeechDemo.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace DependencyService
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TextToSpeechDemo : ContentPage
+    {
+        public TextToSpeechDemo()
+        {
+            InitializeComponent();
+            var stack = new StackLayout();
+            var speak = new Button
+            {
+                Text = "Hello,Valdez Forms!",
+                VerticalOptions = LayoutOptions.CenterAndExpand,
+                HorizontalOptions = LayoutOptions.CenterAndExpand,
+            };
+
+            speak.Clicked += (sender, e) =>
+            {
+                Xamarin.Forms.DependencyService.Get<ITextToSpeech>().Speak("Hello Valdez from Xamarin Forms");
+            };
+
+            stack.Children.Add(speak);
+            Content = speak;
+        }
+    }
+}
